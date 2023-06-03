df5e4d335ae05557be174e77afcd0567d236b7a7
= <=>
2023-06-03 15:52:24 +0200

    Fist attemp to return results from query

--- api/sixt.py
+++ api/sixt.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env
-
+import requests
+import pendulum
 from urllib.parse import urlparse, urlencode, parse_qs
-from pprint import pprint
-from utils.dates import get_days_groups_yearly_from_now
+from api.utils.dates import get_days_groups_yearly_from_now
 
 # Base URL
 base_url="https://web-api.orange.sixt.com/v1/rentaloffers/offers?pickupStation=S_40106&returnStation=S_40106&pickupDate=2023-02-17T17:00:00&returnDate=2023-02-20T17:00:00&vehicleType=car&currency=EUR&isoCountryCode=FR"
@@ -11,16 +10,17 @@ base_url="https://web-api.orange.sixt.com/v1/rentaloffers/offers?pickupStation=S
 url_parts = urlparse(base_url)
 query_params = parse_qs(url_parts.query)
 
-# # Modify the query parameters
-# query_params['pickupStation'] = 'S_40106'
-# query_params['returnStation'] = 'S_40106'
-# query_params['pickupDate'] = '2023-05-20T10:00:00'
-# query_params['returnDate'] = '2023-05-23T10:00:00'
-
-# url_parts = url_parts._replace(query=urlencode(query_params, doseq=True))
-# modified_url = url_parts.geturl()
-
-
+# Get All stations list
+def stations_list(): 
+#    station = "S_40106"
+   response = requests.get(generate_url_yearly(3)[2])
+   data = response.json()
+   data = data['offers'][0]
+   stations = [str("S_")+ str(i).zfill(5) for i in range(0, 99999)]
+   stations_length = len(stations)
+   return data
+   
+# Generate URL for a givent number of days.
 def generate_url_yearly(days: int):
     urls = []
     for day in get_days_groups_yearly_from_now(days):
@@ -31,5 +31,39 @@ def generate_url_yearly(days: int):
         urls.append(item)
     return urls
 
-pprint(generate_url_yearly(2))
- 
+
+# Get finals results
+def compare_results(days: int = 3):
+   # Fetch results for the second item of the url list generate_url_yearly(3)[1]
+   response = requests.get(generate_url_yearly(days)[2])
+   data = response.json()
+
+#    print(generate_url_yearly(days)[2])
+   # Get pendulum date from data['info']['pickupDate'] and data['info']['returnDate']
+   pickupDate = pendulum.parse(data['info']['pickupDate'])
+   returnDate = pendulum.parse(data['info']['returnDate'])
+   infos = {
+         'pickupStation': data['info']['pickupStationId'],
+         'returnStation': data['info']['returnStationId'],
+         'pickupDate': pickupDate.format("DD/MM/YYYY"),
+         'returnDate': returnDate.format("DD/MM/YYYY"),
+         'duration': pickupDate.diff_for_humans(returnDate, absolute=True),
+   }
+   # Generate a list of offers of one url and put 
+   offers = []
+   for offer in data['offers']:
+       offers.append({
+           'id': offer['id'], 
+           'status': offer['status'], 
+           'description': offer['description'],
+           'maxDistance': offer['mileageInfo']['display'],
+           'maxPassengers': offer['vehicleGroupInfo']['maxPassengers'],
+           'dayPrice': offer['prices']['dayPrice']['amount']["value"],
+           'basePrice': offer['prices']['totalPrice']['amount']["value"],
+           })
+    
+   # Return the list of offers sorted by dayPrice
+   return {
+       'infos': infos,
+       'offers': sorted(offers, key=lambda k: k['dayPrice'])
+       }
--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, APIRouter
 
-from api.sixt import generate_url_yearly
+from api.sixt import compare_results
+
+router = APIRouter()
+
+# from api.sixt import generate_url_yearly
+from api.sixt import stations_list as StationsList
 
 app = FastAPI()
 
 # API endpoint
 @app.get("/")
 def read_root():
-  return "ok"
+  return "Welcome to Beluga API"
 
-
-
-# @app.get("/rent/vehicles/sixt/bulk/{num_days}")
-# def read_item(num_days: int = 3):
-#     return generate_url_yearly(num_days)
+@app.get("/rent/vehicles/sixt/bulk/{num_days}")
+def read_item(num_days: int = 3):
+    return compare_results(num_days)
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": "On c'est pas trop ce que c'est"}
+    return {"item_id": item_id, "q": "You called items number " + str(item_id) + " with query " + str(q)}
